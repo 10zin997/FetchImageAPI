f4dcbf506757d0eac5f7e19721c0b0e8119548a5
Tenzin wangyal <tenzinwangyal20@yahoo.com>
2022-11-15 15:08:49 -0800

    Initial Commit

---
+++ API/APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIApp.swift
+//  API
+//
+//  Created by Tenzin wangyal on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
